a737c11f086c8a59223bd82d94448cc8b19e66ac
MarkFrid <mark.fridlin@gmail.com>
2025-04-09 12:33:29 -0400

    asd

--- app/src/components/TeacherButton.vue
+++ app/src/components/TeacherButton.vue
@@ -1,12 +1,12 @@
+<script setup>
+import frusciHead from '../assets/frusciHead.png'
+</script>
+
 <template>
   <div>
     <h1>asd</h1>
-    <button><img src="../public/teacher.png" /></button>
+    <button><img :src="frusciHead" /></button>
   </div>
 </template>
 
-<script setup>
-let grade = 0
-</script>
-
-<style scoped></style>
+<style></style>
