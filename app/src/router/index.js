51042f7cda3843d5fc1b4c0abb47abab78a110db
cchuuu <158204289+cchuuu@users.noreply.github.com>
2025-04-11 12:30:10 -0400

    a

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import LoginView from '@/views/LoginView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'Login',
+      component: LoginView,
     },
     {
       path: '/about',
---
+++ app/src/views/LoginView.vue
@@ -0,0 +1,35 @@
+<template>
+    <h1 class="text-l">Sign In Page</h1>
+    <div class="items-center">
+    <div class="userInput">
+        <label for="username"> Username: </label>
+        <input type="username" id="username" v-model="username">
+    </div>
+    <div class="passwordInput">
+        <label for="password"> Password: </label>
+        <input type="password" id="password" v-model="password">
+    </div>
+    <div class="buttonContainer">
+        <button @click="signIn"></button>
+        <button @click="signUp"></button>
+    </div>
+</div>
+</template>
+  
+<script setup>
+import { ref } from 'vue';
+let password = ref("");
+let username = ref("");
+
+function signIn() {
+    console.log("sign in")
+}
+ 
+function signUp() {
+    console.log("sign up")
+}
+</script>
+<style scoped>
+
+</style>
+  
