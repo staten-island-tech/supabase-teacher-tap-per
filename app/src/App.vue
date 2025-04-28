b241540253641c4543ccebf0fe723cf28f5dd77d
MarkFrid <mark.fridlin@gmail.com>
2025-04-28 12:33:37 -0400

    frick CHU

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,9 @@
 <script setup>
-<<<<<<< HEAD
 import { RouterLink, RouterView } from 'vue-router'
-=======
-import LoginView from './views/LoginView.vue';
->>>>>>> main
 </script>
 
 <template>
-<LoginView></LoginView>
+  <LoginView></LoginView>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
