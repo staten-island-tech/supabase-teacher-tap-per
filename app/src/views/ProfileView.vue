b544e63a490f683a9293075c807cda939c496ec5
cchuuu <158204289+cchuuu@users.noreply.github.com>
2025-05-13 12:32:34 -0400

    a

--- app/src/views/ProfileView.vue
+++ app/src/views/ProfileView.vue
@@ -4,13 +4,16 @@
             <h1>You need to be logged in to use this feature.</h1>
         </div>
         <div v-if="user">
-            <div class="container">Email: ${currentSession.data.session.user.email}</div>
+            <div class="container">
+                <h2>Email: {{ user.email }}</h2>
+            </div>
         </div>
     </div>
     </template>
     
     <script setup>
     import { user } from '@/useAuth';
+
     
     </script>
     <style scoped>
