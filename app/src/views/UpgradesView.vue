685860d37280f9bc2e67f51dc9258f7e90e61df8
cchuuu <158204289+cchuuu@users.noreply.github.com>
2025-05-27 12:31:29 -0400

    Update UpgradesView.vue

--- app/src/views/UpgradesView.vue
+++ app/src/views/UpgradesView.vue
@@ -16,7 +16,7 @@
 
               <div class="flex items-center space-x-4 md:space-x-6">
                 <img
-                  src="person.image"
+                  :src="person.image"
                   :alt="person.name"
                   class="w-12 h-12 md:w-16 md:h-16 rounded-full"
                 />
@@ -28,10 +28,10 @@
 
               <div class="flex flex-col items-end mt-4 md:mt-0 space-y-2">
                 <div class="text-right">
-                  <p class="font-semibold text-xl">Current Count: 0</p>
-                  <p class="text-sm text-gray-500">Upgrades: 0</p>
+                  <p class="font-semibold text-xl">Current Count: {{ person.count }}</p>
+                  <p class="text-sm text-gray-500">Upgrades: +{{ person.upgrades }}</p>
                 </div>
-                <button class="bg-blue-500 text-white py-2 px-4 rounded-lg">
+                <button @click="upgradeButton(person)" class="bg-blue-500 text-white py-2 px-4 rounded-lg">
                   Upgrade - ${{ person.cost }}
                 </button>
               </div>
@@ -46,15 +46,23 @@
     
 <script setup>
 import { user } from '@/useAuth';
-const upgrades = [
-      { name: "Barone", adds: 1, cost: 1, image: "BaroneAlison.jpg" },
-      { name: "Colangelo", adds: 2, cost: 2, image: "ColangeloJonathan.jpg" },
-      { name: "Frusci", adds: 5, cost: 5, image: "FrusciJoseph.jpg" },
-      { name: "Partnow", adds: 10, cost: 10, image: "Partnow.jpg" },
-      { name: "Nickolauk", adds: 25, cost: 25, image: "Nick.png" },
-      { name: "Amerosa", adds: 50, cost: 50, image: "https://via.placeholder.com/40" },
-      { name: "Whalen", adds: 100, cost: 100, image: "WhalenMichael.jpg" },
-]
+import { ref } from 'vue';
+const upgrades = ref([
+      { name: "Barone", adds: 1, cost: 1, count: 0, upgrades: 0, image: "BaroneAlison.jpg" },
+      { name: "Colangelo", adds: 2, cost: 10, count: 0, upgrades: 0, image: "ColangeloJonathan.jpg" },
+      { name: "Frusci", adds: 5, cost: 100, count: 0, upgrades: 0, image: "FrusciJoseph.jpg" },
+      { name: "Partnow", adds: 10, cost: 500, count: 0, upgrades: 0, image: "Partnow.jpg" },
+      { name: "Nickolauk", adds: 25, cost: 1000, count: 0, upgrades: 0, image: "Nick.png" },
+      { name: "Amerosa", adds: 50, cost: 5000, count: 0, upgrades: 0, image: "https://via.placeholder.com/40" },
+      { name: "Whalen", adds: 100, cost: 15000, count: 0, upgrades: 0, image: "WhalenMichael.jpg" },])
+
+function upgradeButton(x) {
+  x.cost = Math.round((x.cost * 1.04 * 100)) / 100
+
+  x.count = x.count + 1
+
+  x.upgrades = x.upgrades + x.adds
+}
 
 </script>
 <style scoped>
