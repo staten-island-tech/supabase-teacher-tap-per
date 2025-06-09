aef8e1ac470e1fe66eb91de5d69af68b98517380
cchuuu <158204289+cchuuu@users.noreply.github.com>
2025-06-09 12:22:32 -0400

    a

---
+++ app/src/components/UpgradeList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <div v-if="!authStore.user">
+        <h1>You need to be logged in to use this feature.</h1>
+      </div>
+      <div v-if="authStore.user">
+        <div class="bg-gray-100 p-8">
+          <div class="bg-gray-100 p-4 md:p-8">
+            <div class="max-w-4xl mx-auto space-y-4">
+                <slot>
+                    
+                </slot>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script setup>
+import { useAuthStore } from '@/stores/useAuth';
+const authStore = useAuthStore();
+</script>
--- app/src/views/UpgradesView.vue
+++ app/src/views/UpgradesView.vue
@@ -1,54 +1,42 @@
 <template>
-<div>
-  <div v-if="!authStore.user">
-    <h1>You need to be logged in to use this feature.</h1>
+<UpgradeList>
+  <div
+    v-for="person in upgradeStore.upgrades"
+    :key="person.name" 
+    class="flex flex-col md:flex-row items-center justify-between p-4 bg-white rounded-lg shadow-md"
+  >
+
+  <div class="flex items-center space-x-4 md:space-x-6">
+  <img
+    :src="person.image"
+    :alt="person.name"
+    class="w-12 h-12 md:w-16 md:h-16 rounded-full"
+  />
+  <div>
+  <p class="text-lg font-semibold">{{ person.name }}</p>
+  <p class="text-sm text-gray-500">Adds: {{ person.adds }}</p>
   </div>
-  <div v-if="authStore.user">
-    <div class="bg-gray-100 p-8">
-      <div class="bg-gray-100 p-4 md:p-8">
-        <div class="max-w-4xl mx-auto space-y-4">
-          <div class="space-y-4">
-            <div
-              v-for="person in upgradeStore.upgrades"
-              :key="person.name"
-              class="flex flex-col md:flex-row items-center justify-between p-4 bg-white rounded-lg shadow-md"
-            >
-
-              <div class="flex items-center space-x-4 md:space-x-6">
-                <img
-                  :src="person.image"
-                  :alt="person.name"
-                  class="w-12 h-12 md:w-16 md:h-16 rounded-full"
-                />
-                <div>
-                  <p class="text-lg font-semibold">{{ person.name }}</p>
-                  <p class="text-sm text-gray-500">Adds: {{ person.adds }}</p>
-                </div>
-              </div>
-
-              <div class="flex flex-col items-end mt-4 md:mt-0 space-y-2">
-                <div class="text-right">
-                  <p class="font-semibold text-xl">Current Count: {{ person.count }}</p>
-                  <p class="text-sm text-gray-500">Current Stats: +{{ person.upgrades }}</p>
-                </div>
-                <button @click="upgradeStore.upgradeButton(person)" class="bg-blue-500 text-white py-2 px-4 rounded-lg">
-                  Upgrade - ${{ person.cost.toFixed(2) }}
-
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
+
+    <div class="flex flex-col items-end mt-4 md:mt-0 space-y-2">
+    <div class="text-right">
+    <p class="font-semibold text-xl">Current Count: {{ person.count }}</p>
+    <p class="text-sm text-gray-500">Current Stats: +{{ person.upgrades }}</p>
+
+  </div>
+    <button @click="upgradeStore.upgradeButton(person)" class="bg-blue-500 text-white py-2 px-4 rounded-lg">
+    Upgrade - ${{ person.cost.toFixed(2) }}
+    </button>
+   </div>
 </div>
+</UpgradeList>
 </template>
     
 <script setup>
 import { useUpgradeStore } from '@/stores/useUpgrade';
 import { useAuthStore } from '@/stores/useAuth';
 import { onMounted, watch } from 'vue';
+import UpgradeList from '@/components/UpgradeList.vue';
 
 const upgradeStore = useUpgradeStore();
 const authStore = useAuthStore();
