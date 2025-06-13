3e3a8443b82e52d1805018f2e9603c25214c7d20
MarkFrid <mark.fridlin@gmail.com>
2025-06-13 11:50:31 -0400

    a

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
     <div v-if="!user">
-      <h1>You need to be logged in</h1>
+      <h1>You need to be logged in to use this feature.</h1>
     </div>
     <div v-else>
-      <h1>Welcome, {{ user.email }}</h1>
       <div v-if="user"><GradeCounter /> <TeacherButton /></div>
     </div>
   </div>
